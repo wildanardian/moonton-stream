e9cd1a464ac2ab3eb77a5ba20e3b6cafb5f7b8be
Dedy Rahmat <acesonix@gmail.com>
2023-08-25 15:26:04 +0700

    feat: Setup Spatie

---
+++ database/seeders/UserTableSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UserTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $admin = User::create([
+            'name' => 'Admin Website',
+            'email' => 'admin@gmail.com',
+            'password' => bcrypt('password'),
+        ]);
+        $admin->assignRole('admin');
+    }
+}
